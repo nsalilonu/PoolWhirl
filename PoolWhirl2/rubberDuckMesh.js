0d29535c1fd687bf6813e92ad469de0b15dcaf1a
nsalilonu <nalilonu@princeton.edu>
2020-05-02 23:16:39 -0700

    So we have water, a rubber duck, and weird camera
    Co-authored-by: kiwikarrot <rachellee21999@gmail.com>
    Co-authored-by: admeliora <yin.emily@yahoo.com>
    Co-authored-by: seanwynwins <seanwynng@gmail.com>

---
+++ PoolWhirl2/rubberDuckMesh.js
@@ -0,0 +1,57 @@
+let duckHeadGeometry = new THREE.BoxGeometry(14, 12, 12);
+
+// duck material which is set to a golden yellow
+let duckMaterial = new THREE.MeshPhongMaterial({color: 0xFFC300})
+
+// stores the entirety of the duck
+let duckMesh = new THREE.Mesh(duckHeadGeometry, duckMaterial);
+
+// make the right eye
+let duckEye = new THREE.BoxGeometry(2.5, 2.5, 1);
+let eyeMaterial = new THREE.MeshPhongMaterial({color: 0x080200});
+let rightEye = new THREE.Mesh(duckEye, eyeMaterial);
+rightEye.translateX(-4);
+rightEye.translateY(2);
+rightEye.translateZ(-6);
+duckMesh.add(rightEye);
+
+// make the left eye
+let leftEye = rightEye.clone();
+leftEye.translateX(8);
+duckMesh.add(leftEye);
+
+// create the duck's bill
+let billGeometry = new THREE.BoxGeometry(10, 2, 5);
+let billMaterial = new THREE.MeshPhongMaterial({color: 0xFF7919});
+let bill = new THREE.Mesh(billGeometry, billMaterial);
+bill.translateY(-2);
+bill.translateZ(-10);
+duckMesh.add(bill);
+
+// create the duck's body
+let bodyGeometry = new THREE.BoxGeometry(10, 12, 18);
+let body = new THREE.Mesh(bodyGeometry, duckMaterial);
+body.translateY(-12);
+body.translateZ(10);
+duckMesh.add(body);
+
+// add a tail
+let tailGeometry = new THREE.BoxGeometry(2, 2, 2);
+let tail = new THREE.Mesh(tailGeometry, duckMaterial);
+tail.translateZ(18);
+tail.translateY(-2);
+duckMesh.add(tail);
+
+// create the wings
+let wingGeometry = new THREE.BoxGeometry(2, 9, 12);
+let rightWing = new THREE.Mesh(wingGeometry, duckMaterial);
+rightWing.translateY(-10);
+rightWing.translateX(7);
+rightWing.translateZ(18);
+duckMesh.add(rightWing);
+
+let leftWing = rightWing.clone();
+leftWing.translateX(-14);
+duckMesh.add(leftWing);
+
+export {duckMesh};
--- PoolWhirl2/teddyMesh.js
+++ PoolWhirl2/teddyMesh.js
@@ -1,7 +1,154 @@
 let teddyHeadGeometry = new THREE.BoxGeometry(14,12,12);
 let teddyMaterial = new THREE.MeshPhongMaterial({color: 0xBF8975});
-let teddyMesh = new THREE.Mesh(teddyHeadGeometry, teddyMaterial);
-let teddyEyeGeometry = new THREE.BoxGeometry(4,4,1);
-let teddyPupilGeometry = new THREE.BoxGeometry(2,2,1)
 
-export {teddyMesh};
+let teddyMesh = new THREE.Mesh(teddyHeadGeometry, teddyMaterial);
+let teddyEyeGeometry = new THREE.BoxGeometry(2.5,2.5,1);
+let teddyEyeMaterial = new THREE.MeshPhongMaterial({color: 0xffffff});
+let teddyRightEyeMesh = new THREE.Mesh(teddyEyeGeometry, teddyEyeMaterial);
+teddyRightEyeMesh.translateX(-4);
+teddyRightEyeMesh.translateZ(-6);
+teddyMesh.add(teddyRightEyeMesh);
+
+let teddyLeftEyeMesh = teddyRightEyeMesh.clone();
+teddyLeftEyeMesh.translateX(8);
+teddyMesh.add(teddyLeftEyeMesh);
+
+let teddyRightPupilMaterial = new THREE.MeshPhongMaterial({color: 0x000000});
+let teddyRightPupilGeometry = new THREE.BoxGeometry(1.5,1.5, 0.5);
+let teddyRightPupil = new THREE.Mesh(teddyRightPupilGeometry, teddyRightPupilMaterial);
+// teddyRightPupil.translateX(-4);
+ teddyRightPupil.translateZ(-0.5);
+teddyRightEyeMesh.add(teddyRightPupil);
+
+let teddyLeftPupil = teddyRightPupil.clone();
+// teddyLeftPupil.translateX(8);
+teddyLeftEyeMesh.add(teddyLeftPupil);
+
+let teddySnoutGeometry = new THREE.BoxGeometry(6,6,2);
+let teddySnoutMaterial = new THREE.MeshPhongMaterial({color: 0xffdfd3});
+let teddySnout = new THREE.Mesh(teddySnoutGeometry, teddySnoutMaterial);
+teddySnout.translateY(-4);
+teddySnout.translateZ(-7);
+teddyMesh.add(teddySnout);
+
+let teddyNoseGeometry = new THREE.BoxGeometry(3,3,1);
+let teddyNose = new THREE.Mesh(teddyNoseGeometry, teddyRightPupilMaterial);
+teddyNose.translateY(1.5);
+teddyNose.translateZ(-1.5);
+teddySnout.add(teddyNose);
+
+let teddyRightEarGeometry = new THREE.BoxGeometry(4, 4, 1);
+let teddyRightEarMesh = new THREE.Mesh(teddyRightEarGeometry, teddyMaterial);
+teddyRightEarMesh.translateZ(-5.5);
+teddyRightEarMesh.translateY(9);
+teddyRightEarMesh.translateX(-8);
+teddyMesh.add(teddyRightEarMesh);
+
+let teddyLeftEarMesh = teddyRightEarMesh.clone();
+teddyRightEarMesh.translateX(16);
+teddyMesh.add(teddyLeftEarMesh);
+
+let teddyLeftEarPatchGeometry = new THREE.BoxGeometry(3, 3, 0.5);
+let teddyLeftEarPatch = new THREE.Mesh(teddyLeftEarPatchGeometry, teddySnoutMaterial);
+teddyLeftEarPatch.translateZ(-0.75);
+teddyLeftEarMesh.add(teddyLeftEarPatch);
+
+let teddyRightEarPatch = new THREE.Mesh(teddyLeftEarPatchGeometry, teddySnoutMaterial);
+teddyRightEarPatch.translateZ(-0.75);
+teddyRightEarMesh.add(teddyRightEarPatch);
+
+let teddyBodyGeometry = new THREE.BoxGeometry(10, 12, 12);
+let teddyBody = new THREE.Mesh(teddyBodyGeometry, teddyMaterial);
+teddyBody.translateY(-12.5);
+teddyMesh.add(teddyBody);
+
+let teddyBodyPatchGeometry = new THREE.BoxGeometry(7, 9, 0.5);
+let teddyBodyPatch = new THREE.Mesh(teddyBodyPatchGeometry, teddySnoutMaterial);
+teddyBodyPatch.translateZ(-6.25);
+teddyBody.add(teddyBodyPatch);
+
+let teddyRightArmGeometry = new THREE.BoxGeometry(5, 8, 5);
+let teddyRightArm = new THREE.Mesh(teddyRightArmGeometry, teddyMaterial);
+teddyRightArm.translateX(-10);
+teddyRightArm.translateY(2);
+teddyRightArm.rotateZ(-Math.PI / 4);
+teddyBody.add(teddyRightArm);
+
+let teddyLeftArmGeometry = new THREE.BoxGeometry(5, 8, 5);
+let teddyLeftArm = new THREE.Mesh(teddyLeftArmGeometry, teddyMaterial);
+teddyLeftArm.translateX(10);
+teddyLeftArm.translateY(2);
+teddyLeftArm.rotateZ(Math.PI / 4);
+teddyBody.add(teddyLeftArm);
+
+let teddyLeftArmPatchGeometry = new THREE.BoxGeometry(3, 3, 0.5);
+let teddyLeftArmPatch = new THREE.Mesh(teddyLeftArmPatchGeometry, teddySnoutMaterial);
+teddyLeftArmPatch.translateY(-4.25);
+teddyLeftArmPatch.rotateX(-Math.PI/2);
+teddyLeftArm.add(teddyLeftArmPatch);
+
+let teddyRightArmPatchGeometry = new THREE.BoxGeometry(3, 3, 0.5);
+let teddyRightArmPatch = new THREE.Mesh(teddyRightArmPatchGeometry, teddySnoutMaterial);
+teddyRightArmPatch.translateY(-4.25);
+ teddyRightArmPatch.rotateX(-Math.PI/2);
+teddyRightArm.add(teddyRightArmPatch);
+
+let teddyRightLegGeometry = new THREE.BoxGeometry(5, 10, 5);
+let teddyRightLeg = new THREE.Mesh(teddyRightLegGeometry, teddyMaterial);
+teddyRightLeg.translateX(-9);
+teddyRightLeg.translateY(-7.75);
+teddyRightLeg.translateZ(-2);
+teddyRightLeg.rotateX(Math.PI / 2);
+teddyRightLeg.rotateY(Math.PI / 8);
+teddyRightLeg.rotateZ(-Math.PI / 8);
+teddyBody.add(teddyRightLeg);
+
+let teddyLeftLegGeometry = new THREE.BoxGeometry(5, 10, 5);
+let teddyLeftLeg = new THREE.Mesh(teddyLeftLegGeometry, teddyMaterial);
+teddyLeftLeg.translateX(9);
+teddyLeftLeg.translateY(-7.75);
+teddyLeftLeg.translateZ(-2);
+teddyLeftLeg.rotateX(Math.PI / 2);
+teddyLeftLeg.rotateY(-Math.PI / 8);
+teddyLeftLeg.rotateZ(Math.PI / 8);
+teddyBody.add(teddyLeftLeg);
+
+let teddyLeftLegPatchGeometry = new THREE.BoxGeometry(3, 3, 0.5);
+let teddyLeftLegPatch = new THREE.Mesh(teddyLeftLegPatchGeometry, teddySnoutMaterial);
+teddyLeftLegPatch.translateY(-5.25);
+teddyLeftLegPatch.rotateX(-Math.PI/2);
+teddyLeftLeg.add(teddyLeftLegPatch);
+
+let teddyRightLegPatchGeometry = new THREE.BoxGeometry(3, 3, 0.5);
+let teddyRightLegPatch = new THREE.Mesh(teddyRightLegPatchGeometry, teddySnoutMaterial);
+teddyRightLegPatch.translateY(-5.25);
+ teddyRightLegPatch.rotateX(-Math.PI/2);
+teddyRightLeg.add(teddyRightLegPatch);
+
+let teddyTailGeometry = new THREE.BoxGeometry(3, 3, 3);
+let teddyTail = new THREE.Mesh(teddyTailGeometry, teddyMaterial);
+teddyTail.translateY(-4.5);
+teddyTail.translateZ(8);
+teddyBody.add(teddyTail);
+
+var timer = 0;
+function teddyInit() {
+    timer = 0;
+}
+
+function teddyMunch() {
+timer+= Math.PI/4 /10;
+if (timer > 0 && timer <= Math.PI/4) {
+    teddyRightArm.rotateX(Math.PI/4 / 10);
+}
+else if (timer > 0 && timer > Math.PI/4 && timer < Math.PI/2)
+{
+teddyRightArm.rotateX(-Math.PI/4 / 10);
+} 
+}
+
+
+
+
+
+export {teddyMesh, teddyMunch, teddyInit};
